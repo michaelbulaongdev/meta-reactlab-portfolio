6936db406465cadb1088437ae0db5def3d1a408b
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-26 07:45:09 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faEnvelope, faHouseUser} from '@fortawesome/free-solid-svg-icons';
 import {
@@ -7,7 +7,7 @@ import {
 	faMedium,
 	faStackOverflow,
 } from '@fortawesome/free-brands-svg-icons';
-import {Box, HStack} from '@chakra-ui/react';
+import {Box, HStack, Slide} from '@chakra-ui/react';
 import {Link} from 'react-scroll';
 
 const socials = [
@@ -33,7 +33,8 @@ const socials = [
 	},
 ];
 
-const Header = () => {
+export default function Header() {
+	//disabled the handleClick and replaced with react-scroll
 	// const handleClick = (anchor) => () => {
 	//   const id = `${anchor}-section`;
 	//   const element = document.getElementById(id);
@@ -44,76 +45,93 @@ const Header = () => {
 	//     });
 	//   }
 	// };
-	//disabled the handleClick and replaced with react-scroll
+	const [show, setShow] = useState(true);
+	const [prevScrollY, setPrevScrollY] = useState(0);
+
+	const handleScroll = () => {
+		window.scrollY > prevScrollY ? setShow(false) : setShow(true);
+		setPrevScrollY(window.scrollY);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, [prevScrollY]);
+
+	const offsetValue = -20;
 
 	return (
-		<Box
-			position='fixed'
-			top={0}
-			left={0}
-			right={0}
-			translateY={0}
-			transitionProperty='transform'
-			transitionDuration='.3s'
-			transitionTimingFunction='ease-in-out'
-			backgroundColor='#18181b'
-			zIndex={3}>
-			<Box color='white' maxWidth='1280px' margin='0 auto'>
-				<HStack
-					px={16}
-					py={4}
-					justifyContent='space-between'
-					alignItems='center'>
-					<nav>
-						<HStack spacing='20px'>
-							{socials.map((item) => {
-								return (
-									<a
-										key={item.url}
-										href={item.url}
-										target='_blank'
-										rel='noreferrer'>
-										<FontAwesomeIcon icon={item.icon} size='2x' />
-									</a>
-								);
-							})}
-						</HStack>
-					</nav>
-					<nav>
-						<HStack spacing={8}>
-							<Link
-								activeClass='active'
-								className='nav-scroll'
-								to='landing-section'
-								offset={-80}
-								smooth={true}
-								duration={500}>
-								<FontAwesomeIcon icon={faHouseUser} size='1x' />
-							</Link>
-							<Link
-								activeClass='active'
-								className='nav-scroll'
-								to='projects-section'
-								ignoreCancelEvents={true}
-								offset={-80}
-								smooth={true}
-								duration={500}>
-								Projects
-							</Link>
-							<Link
-								activeClass='active'
-								className='nav-scroll'
-								to='contactme-section'
-								offset={-80}
-								smooth={true}
-								duration={500}>
-								Contact Me
-							</Link>
-						</HStack>
-					</nav>
-				</HStack>
+		<Slide direction='top' in={show} unmountOnExit={true} style={{zIndex: 10}}>
+			<Box
+				position='fixed'
+				top={0}
+				left={0}
+				right={0}
+				// translateY={0}
+				transitionProperty='transform'
+				transitionDuration='.3s'
+				transitionTimingFunction='ease-in-out'
+				backgroundColor='#18181b'
+				zIndex={3}>
+				<Box color='white' maxWidth='1280px' margin='0 auto'>
+					<HStack
+						px={16}
+						py={4}
+						justifyContent='space-between'
+						alignItems='center'>
+						<nav>
+							<HStack spacing='20px'>
+								{socials.map((item) => {
+									return (
+										<a
+											key={item.url}
+											href={item.url}
+											target='_blank'
+											rel='noreferrer'>
+											<FontAwesomeIcon icon={item.icon} size='2x' />
+										</a>
+									);
+								})}
+							</HStack>
+						</nav>
+						<nav>
+							<HStack spacing={8}>
+								<Link
+									activeClass='active'
+									className='nav-scroll'
+									to='landing-section'
+									offset={offsetValue}
+									smooth={true}
+									duration={500}>
+									<FontAwesomeIcon icon={faHouseUser} size='1x' />
+								</Link>
+								<Link
+									activeClass='active'
+									className='nav-scroll'
+									to='projects-section'
+									ignoreCancelEvents={true}
+									offset={offsetValue}
+									smooth={true}
+									duration={500}>
+									Projects
+								</Link>
+								<Link
+									activeClass='active'
+									className='nav-scroll'
+									to='contactme-section'
+									offset={offsetValue}
+									smooth={true}
+									duration={500}>
+									Contact Me
+								</Link>
+							</HStack>
+						</nav>
+					</HStack>
+				</Box>
 			</Box>
-		</Box>
+			//{' '}
+		</Slide>
 	);
-};
-export default Header;
+}
